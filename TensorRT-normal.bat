228b957aad8cacc31632b00ac7063c1eeacdada7
JackEllie <119565920+JackEllie@users.noreply.github.com>
2023-11-03 22:33:43 +0800

    Add files via upload

---
+++ TensorRT-normal.bat
@@ -0,0 +1,10 @@
+@echo off
+
+call .\venv\Scripts\activate.bat
+
+call python.exe -m pip install --upgrade pip
+call python -m pip install nvidia-cudnn-cu11==8.9.4.25 --no-cache-dir
+call python -m pip install --pre --extra-index-url https://pypi.nvidia.com/ tensorrt==9.0.1.post11.dev4 --no-cache-dir
+call python -m pip uninstall -y nvidia-cudnn-cu11
+
+pause
